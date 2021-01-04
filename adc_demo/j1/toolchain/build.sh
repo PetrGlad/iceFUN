a065e5f03a34f4f91067566014652eef12f98434
Petr Gladkikh <petrglad@gmail.com>
2021-01-04 21:35:04 +0100

    Fix firmware build procedure

--- adc_demo/j1/toolchain/build.sh
+++ adc_demo/j1/toolchain/build.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
-set -e
+set -ex
 HERE=$(dirname $0)
 SOURCE=$(realpath $1)
-TARGET=$(realpath $2)
+TARGET=$(realpath -m $2)
 
 cd $HERE
 mkdir --parents "build"
 rm --force build/*
-cp "$SOURCE" .
-gforth cross.fs basewords.fs "$(basename $SOURCE)"
+
+## It seems that cross.fs does not support sources outsdide of current directory.
+## Working around that for now.
+SOURCE_FILE="build-$(basename $SOURCE)"
+cp "$SOURCE" "./$SOURCE_FILE"
+
+gforth cross.fs basewords.fs "$SOURCE_FILE"
+
 mkdir --parents "$(dirname $TARGET)"
+cp "./build/$(basename --suffix=.fs $SOURCE_FILE).hex" "$TARGET"
+rm "$SOURCE_FILE"
