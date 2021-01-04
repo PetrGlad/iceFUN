066f1655dc830c5465e183c20e7421ce87f25a59
Petr Gladkikh <petrglad@gmail.com>
2021-01-04 22:54:06 +0100

    Trying Forth loops
    Because I can.

--- adc_demo/icefun-fw.fs
+++ adc_demo/icefun-fw.fs
@@ -1,13 +1,12 @@
 : boot
     begin
-        h# 0 io@
-        h# 10 io!
-        h# 1 io@
-        h# 11 io!
-        h# 2 io@
-        h# 12 io!
-        h# 3 io@
-        h# 13 io!
+        3
+        begin
+            dup io@
+            dup h# a + io!
+            -1 +
+            dup 0 <
+        until
     again
 ;
 
